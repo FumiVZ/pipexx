f28848d834f5e296e684d4a079f4496faea8eafe
denos <denos@Desktop>
2024-04-24 22:04:51 +0200

    norm wildcard

--- code/split_command/wildcard.c
+++ code/split_command/wildcard.c
@@ -6,35 +6,35 @@
 /*   By: machrist <machrist@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:57:00 by machrist          #+#    #+#             */
-/*   Updated: 2024/04/24 18:29:05 by machrist         ###   ########.fr       */
+/*   Updated: 2024/04/24 22:04:06 by machrist         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parthing.h"
 
-static bool	check_is_dir(const char *dir)
+static bool	is_dir(const char *dir, char *pwd)
 {
-	char		*pwd;
 	char		*path;
 	struct stat	info;
 
-	pwd = getcwd(NULL, 0);
 	if (!pwd)
 		return (msg_err(GETCWD));
-	path = ft_strjoin(pwd, "/");
-	free(pwd);
+	path = malloc(sizeof(char) * (ft_strlen(pwd) + ft_strlen(dir) + 2));
 	if (!path)
-		return (msg_err(MALLOC));
-	pwd = ft_strjoin(path, dir);
-	free(path);
-	if (!pwd)
-		return (msg_err(MALLOC));
-	if (stat(pwd, &info) != 0)
 	{
 		free(pwd);
+		return (msg_err(MALLOC));
+	}
+	ft_strlcpy(path, pwd, ft_strlen(pwd) + 1);
+	ft_strlcat(path, "/", ft_strlen(pwd) + 2);
+	ft_strlcat(path, dir, ft_strlen(pwd) + ft_strlen(dir) + 2);
+	free(pwd);
+	if (stat(path, &info) != 0)
+	{
+		free(path);
 		return (msg_err(STAT));
 	}
-	free(pwd);
+	free(path);
 	if (S_ISDIR(info.st_mode))
 		return (true);
 	return (false);
@@ -42,41 +42,29 @@ static bool	check_is_dir(const char *dir)
 
 static bool	match(const char *str, const char *pattern, const char *dir)
 {
-	const char	*next_pattern;
-
 	if (str[0] == '.' && pattern[0] != '.')
 		return (0);
-	next_pattern = pattern + 1;
-	while (*str && *pattern)
+	while ((*str && *pattern) || *pattern == '*')
 	{
 		if (*pattern == '*')
 		{
-			while (*next_pattern == '*')
-				next_pattern++;
-			if (*next_pattern == '\0')
+			while (*pattern == '*')
+				pattern++;
+			if (*pattern == '\0')
 				return (1);
-			while (*str)
-				if (match(str++, next_pattern, dir))
-					return (1);
-			return (0);
 		}
-		else if (*pattern != '?' && *str != *pattern)
+		if (*pattern != '?' && *str != *pattern)
 		{
 			while (*pattern == '/')
-			{
 				pattern++;
-				if (*pattern == '\0' && check_is_dir(dir))
-					return (1);
-			}
+			if (*pattern == '\0' && is_dir(dir, getcwd(NULL, 0))
+				&& ft_strncmp("..", dir, ft_strlen(dir)))
+				return (1);
 			return (0);
 		}
 		str++;
 		pattern++;
 	}
-	while (*pattern == '*')
-		pattern++;
-	if (*pattern == '\0' && *str == '\0')
-		return (1);
 	return (*str == '\0' && *pattern == '\0');
 }
 
@@ -96,7 +84,8 @@ static char	*add_back(char *result, const char *d_name)
 	return (tmp);
 }
 
-static bool	set_result(char **result, const struct dirent *entry, const char *pattern)
+static bool	set_result(char **result, const struct dirent *entry,
+		const char *pattern)
 {
 	const char	*tmp;
 
